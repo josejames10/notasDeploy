d0b228323e2b509162d5a63d5ce843346ad28bc5
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-04-24 22:36:16 -0500

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
-
+app.use(express.static('dist'))
 let notes = [
   {
     id: 1,
@@ -20,8 +20,8 @@ let notes = [
   }
 ]
 const cors = require('cors')
-
 app.use(cors())
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -92,8 +92,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.use(unknownEndpoint)
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
